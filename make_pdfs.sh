f651a24083d13f606c32f1b651ed12866966ef8b
Grégoire Seux <grego_github@familleseux.net>
2024-08-20 19:26:23 +0200

    Build all books in parallel

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -2,9 +2,12 @@
 
 # latexmk hpmor
 
-latexmk hpmor-1
-latexmk hpmor-2
-latexmk hpmor-3
-latexmk hpmor-4
-latexmk hpmor-5
-latexmk hpmor-6
+latexmk hpmor-1 & \
+latexmk hpmor-2 & \
+latexmk hpmor-3 & \
+latexmk hpmor-4 & \
+latexmk hpmor-5 & \
+latexmk hpmor-6 & \
+echo launched, now waiting for all
+
+wait $(jobs -p)
