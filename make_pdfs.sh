485d5560cb122d15c7cffe4c90daad3819f2ff52
Grégoire Seux <grego_github@familleseux.net>
2024-08-20 12:02:44 +0200

    Revert "Build books in parallel"
    This reverts commit 6c6a6dc1fcd6d556d96da8c5ae681b143411c0bc.

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -2,11 +2,9 @@
 
 # latexmk hpmor
 
-# latexmk hpmor-1
-# latexmk hpmor-2
-# latexmk hpmor-3
-# latexmk hpmor-4
-# latexmk hpmor-5
-# latexmk hpmor-6
-
-make -j 8 all
+latexmk hpmor-1
+latexmk hpmor-2
+latexmk hpmor-3
+latexmk hpmor-4
+latexmk hpmor-5
+latexmk hpmor-6
