b853247de67d24a35e82f710f4fbfc36a0b2c3a8
Grégoire Seux <grego_github@familleseux.net>
2024-08-25 16:51:37 +0200

    Adapt to pocket book size

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -2,12 +2,12 @@
 
 # latexmk hpmor
 
-latexmk hpmor-1 & \
-latexmk hpmor-2 & \
-latexmk hpmor-3 & \
-latexmk hpmor-4 & \
-latexmk hpmor-5 & \
-latexmk hpmor-6 & \
+latexmk hpmor-1 > /dev/null 2>&1 & \
+latexmk hpmor-2 > /dev/null 2>&1 & \
+latexmk hpmor-3 > /dev/null 2>&1 & \
+latexmk hpmor-4 > /dev/null 2>&1 & \
+latexmk hpmor-5 > /dev/null 2>&1 & \
+latexmk hpmor-6 > /dev/null 2>&1 & \
 echo launched, now waiting for all
 
 wait $(jobs -p)
