4bab09a290f27c14ae5b8396e2d7bd212b9e00ae
Grégoire Seux <grego_github@familleseux.net>
2024-11-24 11:16:40 +0100

    Corrections chapitre 20

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -3,11 +3,11 @@
 # latexmk hpmor
 
 latexmk hpmor-1 > /dev/null 2>&1 & \
-latexmk hpmor-2 > /dev/null 2>&1 & \
-latexmk hpmor-3 > /dev/null 2>&1 & \
-latexmk hpmor-4 > /dev/null 2>&1 & \
-latexmk hpmor-5 > /dev/null 2>&1 & \
-latexmk hpmor-6 > /dev/null 2>&1 & \
+# latexmk hpmor-2 > /dev/null 2>&1 & \
+# latexmk hpmor-3 > /dev/null 2>&1 & \
+# latexmk hpmor-4 > /dev/null 2>&1 & \
+# latexmk hpmor-5 > /dev/null 2>&1 & \
+# latexmk hpmor-6 > /dev/null 2>&1 & \
 echo launched, now waiting for all
 
 wait $(jobs -p)
