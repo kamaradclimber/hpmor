06311b2ea5dd0c9375f4988ff2e53c8842956aee
Grégoire Seux <grego_github@familleseux.net>
2025-03-03 18:31:51 +0100

    Correct chapitre 22

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -3,7 +3,7 @@
 # latexmk hpmor
 
 latexmk hpmor-1 > /dev/null 2>&1 & \
-# latexmk hpmor-2 > /dev/null 2>&1 & \
+latexmk hpmor-2 # > /dev/null 2>&1 & \
 # latexmk hpmor-3 > /dev/null 2>&1 & \
 # latexmk hpmor-4 > /dev/null 2>&1 & \
 # latexmk hpmor-5 > /dev/null 2>&1 & \
