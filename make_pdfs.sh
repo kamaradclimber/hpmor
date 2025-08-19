e3c8fb60e5a5ccc655cf4652bf2857b07bfdb43d
Grégoire Seux <grego_github@familleseux.net>
2025-08-19 18:00:26 +0200

    Correction chap 51-58

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -3,8 +3,8 @@
 # latexmk hpmor
 
 latexmk hpmor-1 > /dev/null 2>&1 & \
-latexmk hpmor-2 # > /dev/null 2>&1 & \
-# latexmk hpmor-3 > /dev/null 2>&1 & \
+latexmk hpmor-2 > /dev/null 2>&1 & \
+latexmk hpmor-3 # > /dev/null 2>&1 & \
 # latexmk hpmor-4 > /dev/null 2>&1 & \
 # latexmk hpmor-5 > /dev/null 2>&1 & \
 # latexmk hpmor-6 > /dev/null 2>&1 & \
